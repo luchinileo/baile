f457cc43b539f33b828ecc480c522d0bb26d45b5
luchinileo <104324324+luchinileo@users.noreply.github.com>
2022-06-04 23:43:34 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+console.clear();
+
+let personas = [12, 36, 18, 40, 8, 32, 46, 22, 25, 39, 18, 19];
+
+function esMayorQue21(numero) {
+  if (numero > 21) {
+    console.log(numero + " es mayor que 21.");
+  } else {
+    console.log(numero + " no es mayor que 21.");
+  }
+}
+
+//let discoteca = personas.filter(function esMayorQue21 (numero){
+//
+
+//console.log(discoteca);
